52c92e93db2a3d7a00db38d782465fcd2a976280
Nikita Kovalenko <nikita.kov91@gmail.com>
2018-03-14 20:20:30 +0700

    Fix the run

--- src/main/java/runner/App.java
+++ src/main/java/runner/App.java
@@ -16,18 +16,26 @@ public class App {
 
     static Logger logger = LoggerFactory.getLogger(App.class);
 
+    // при необходимости передавать в параметрах
+    public static final String PATH = "C:\\Work\\projects\\GA-on-permutations\\";
+
     public static void main(String[] args) {
         try {
-            List<String> result = new ArrayList<>();
-            for (int i = 0; i <= 99; i++) {
-                Algorithm algorithm = new Algorithm();
-                algorithm.init("C:\\Work\\projects\\GA-on-permutations\\problem1.txt",
-                        "C:\\Work\\projects\\GA-on-permutations\\settings0.txt",
-                        "C:\\Work\\projects\\GA-on-permutations");
-                algorithm.process();
-                result.add(String.valueOf(algorithm.getPopulation().getBestIndividual().getObjectiveFunctionValue()));
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    List<String> result = new ArrayList<>();
+                    for (int k = 0; k <= 99; k++) {
+                        Algorithm algorithm = new Algorithm();
+                        algorithm.init(PATH + "problem" + i + ".txt",
+                                PATH + "settings" + j + ".txt",
+                                PATH);
+                        algorithm.process();
+                        result.add(String.valueOf(algorithm.getPopulation().getBestIndividual().getObjectiveFunctionValue()));
+                    }
+                    Files.write(Paths.get(PATH + "result-problem" + i + "-settings" + j + ".txt"), result);
+                }
             }
-            Files.write(Paths.get("C:\\Work\\projects\\GA-on-permutations\\result-0.txt"), result);
+
         } catch (Exception e) {
             logger.error(e.getMessage());
             e.printStackTrace();
